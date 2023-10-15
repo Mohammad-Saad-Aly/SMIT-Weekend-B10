8cb968e463a861d1345849f06e61a1bc273e74ba
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-10-15 18:06:32 +0500

    new

---
+++ javascript/class 1/app.js
@@ -0,0 +1,30 @@
+// document.write("<h1> hello world </h1>")
+// document.write('<h1>' + 'hello pakistan' + '</h1>')
+// alert("hello world" + "hello pakistan")
+// document.write('hello'+"<br>"+"world")
+// alert('hello'+"\n"+"world")
+
+// var fisrtName = "Mohammad "
+// var middleName = 'Saad '
+// var lastName = 'Ali'
+// document.write(fisrtName + middleName + lastName)
+
+var name1 = 'Ali'  // string
+var age = 24      // int
+var roll_no = 'CSC-20s-088'
+
+document.write(
+    '<table border="1px">'+
+    '<tr>'+
+    '<th>'+"Name"+'</th>'+
+    '<th>'+"AGE"+'</th>'+
+    '<th>'+"ROLL No"+'</th>'+
+    '</tr>'+
+    '<tr>'+
+    '<td>'+ name1 +'</td>'+
+    '<td>'+ age +'</td>'+
+    '<td>'+ roll_no +'</td>'+
+    '</tr>'
+    
+    +'</table>'
+)
---
+++ javascript/class 2/app.js
@@ -0,0 +1,100 @@
+// document.write("2" + "2")
+// plus main 2 khasiyat concat or operatrion
+// plus main agar agey peeche int ho ==> add hojaega
+// plus main agar kisi ek taraf ya phr dono taraf string ho ==> concat hojaega
+
+// var num1 = +prompt('Enter number 1')
+// var num2 = +prompt("Enter number 2")
+// var add = num1 + num2
+// var sub = num1 - num2
+// var mod = num1 % num2
+// var div = num1 / num2
+// var mult = num1 * num2
+
+// document.write(
+
+// "<table border='1px'>"+
+// "<tr>"+
+// "<th>"+"ADD"+"</th>"+
+// "<th>"+"SUB"+"</th>"+
+// "<th>"+"MOD"+"</th>"+
+// "<th>"+"DIV"+"</th>"+
+// "<th>"+"MULT"+"</th>"+
+// "</tr>"+
+
+// "<tr>"+
+// "<td>"+ add +"</td>"+
+// "<td>"+ sub +"</td>"+
+// "<td>"+ mod +"</td>"+
+// "<td>"+ div +"</td>"+
+// "<td>"+ mult +"</td>"+
+// "</tr>"
+
+// +"</table>"
+
+// )
+
+// var eq = 4 + 2 - 5 * 6
+                // 4 +  2 - 30
+                //     6 - 30
+// document.write(eq)
+
+// var num = 2 + 2 * 3 - 3 - (5 * 5)
+                    // 2 + 2 * 3 - 3 - 25 
+                    //     2 + 6 - 3 - 25
+                    //        8 - 3 - 25 
+                    //         5 - 25
+                    //             -20
+// document.write(num)
+
+// post inc   == 5++
+// pre inc    == ++5
+// post dec   == 5--
+// pre dec    == --5   
+
+
+
+
+// var num = 1
+// var eq = num++ + num++ + ++num + num++ + num  
+        //    1   +   2   +   4   +   4   +  5  
+        // document.write(eq)
+
+
+// var num = 5
+// var eq = --num + --num + num 
+//             // 4  +   3   +   3
+// document.write(eq)
+
+// var num1 = 2
+// var num2 = 3
+// var eq = num1++ + num1++ + ++num1 - num2 + num2-- - --num2
+            // 2   +   3    +   5    -   3  +   3    -   1
+            // document.write(eq)
+
+
+var num1 = 4
+var num2 = 2
+var num3 = 3
+var eq = 10 + 4 + num1++ - num1-- + num3++ + ++num3 - --num2 + 2 + num1++ + num2 + num3
+
+var eq2 = num1++ + ++num1 + num2++ + num2-- + num3-- + num3++ + num3-- + --num2 + num1++ + num3++ + ++num1
+
+var eq3 = "abcd" + 2 + 3 + num1++ + "hello" + num2-- + num3-- + 'han bhai' + num3-- + --num2 + 6 - 'Kaisa diya' 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
