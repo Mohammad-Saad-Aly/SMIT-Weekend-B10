ec9d089b0828d9f7d0484bb5d8845a98f63f8191
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-11-19 18:10:25 +0500

    math expression

---
+++ javascript/class 3/app.js
@@ -0,0 +1,29 @@
+
+
+// var num1 = 3
+// var num2 = 4
+// var num3 = "hello world"
+
+// var eq = "abc" + '4' + num2++ + num3++ + -- num3 - --num2 + 10 + undefined  
+//             abc4     +   4    +    NAN +  NAN   -  4  + 10 + undefined
+//                     abc44  + NAN  + 6 + undefined
+//                        abc44+NaN               
+// document.write(eq)
+
+
+
+// function pause(delay) {
+//     var time1 = Date.now();
+//    var time2 = null;
+//     do {
+//         time2 = Date.now();
+//    }
+//    while (time2 - time1 < delay);
+// }  
+
+// document.write('hello')
+// pause(2000)
+// document.write('world')
+
+
+
