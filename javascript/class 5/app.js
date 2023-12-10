a4e87d675727f01440f59144ec1f3f44bb3cbf1e
mohammadsaadali786.92@gmail.com <mohammadsaadali786.92@gmail.com>
2023-12-10 17:42:13 +0500

    loops

---
+++ javascript/class 5/app.js
@@ -0,0 +1,37 @@
+// var arr = ['1',2,3,4,5,6,7,8,9,10]
+// var arr2 = arr.slice(0,5)
+// document.write(arr2)
+
+// falsy values 
+
+// false
+// 0
+// null
+// undefined
+// NaN
+// ''
+
+// var eq = 5 == 4
+// console.log(eq)
+
+// loop
+// for loop
+// while loop
+// do while loop
+
+// for(var i=1; i <= 10; i++){
+//     document.write(i+") Saad <br>")
+// }
+
+// for(var i=1; i<=10; i++){
+//     document.write(`2 x ${i} = ${2*i} <br>`)
+// }
+
+
+// 1) user input => konse table ka print karana hai.
+
+// 2) user input => konsa table or kahan tak.
+//    user input
+
+
+
